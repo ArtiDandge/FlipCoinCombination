e68a34ca7d3701d263200eebfd13f89f052083cc
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-25 17:07:14 +0530

    add triplet and its percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x 
 
 echo "This program displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
-read -p "how many times you want to flip the coin ? " max_flips
+
+#input number of flips respectively
+read -p "how many times you want to flip the for single combination ? " flips_for_singlet
+read -p "how many times you want to flip the for double combination ? " flips_for_doublet
+read -p "how many times you want to flip the for triple combination ? " flips_for_triplet
+
+#dictionary declaretion
 declare -A SingletDictionary
 declare -A DoubletDictionary
+declare -A TripletDictionary
+
 #Variables
 heads=0
 tails=0
@@ -12,7 +20,7 @@ tails=0
 FLIP=$(($((RANDOM%10))%2))
 
 #loop for singlet combination of coins
-for i in `seq $max_flips`
+for i in `seq $flips_for_singlet`
 do
 	FLIP=$(($((RANDOM%10))%2))
 
@@ -29,7 +37,7 @@ do
 done
 
 #loop for doublet combination of coins
-for i in `seq $max_flips`
+for i in `seq $flips_for_doublet`
 do
 	doubletFlip=$(($((RANDOM%4))+1))
 	case $doubletFlip in 
@@ -41,6 +49,23 @@ do
 	esac
 done
 
+for i in `seq $flips_for_triplet`
+do
+        tripletFlip=$(($((RANDOM%8))+1))
+        case $tripletFlip in
+                1)TripletDictionary["key$i"]=`echo "HHH"`;;
+                2)TripletDictionary["key$i"]=`echo "HHT"`;;
+                3)TripletDictionary["key$i"]=`echo "HTH"`;;
+                4)TripletDictionary["key$i"]=`echo "THH"`;;
+		5)TripletDictionary["key$i"]=`echo "TTH"`;;
+                6)TripletDictionary["key$i"]=`echo "THT"`;; 
+		7)TripletDictionary["key$i"]=`echo "HTT"`;;
+		8)TripletDictionary["key$i"]=`echo "TTT"`;;
+		*)TripletDictionary["key$i"]=`echo "NA"`;;
+        esac
+done
+
+
 #display singlet dictionary and its percentage 
 echo -e "Dictionary to show Singlet Combination : \n ${SingletDictionary[@]}"
 echo "Percentage of head win: $head_percent%";
@@ -49,8 +74,15 @@ singlet_count=${#SingletDictionary[@]}
 singlet_percentage=`echo ${#SingletDictionary[@]} | awk 'BEGIN{printf("%0.2f", '$head_percent'+'$tail_percent')}'`
 echo "singlet percentage : $singlet_percentage"
 
-#echo doublet dictionary and its percentage 
+#display doublet dictionary and its percentage 
 echo "Dictionary to show Doublet Combination : ${DoubletDictionary[@]}"
 doublet_count=${#DoubletDictionary[@]}
 doublet_percent=`awk 'BEGIN{printf("%0.2f", '$doublet_count'*100)}'`
 echo "Doublet Percentage : $doublet_percent%"
+
+#display triplet dictionary and its percentage
+echo "Dictionary to show Triplet Combination : ${TripletDictionary[@]}"
+triplet_count=${#TripletDictionary[@]}
+triplet_percent=`awk 'BEGIN{printf("%0.2f", '$triplet_count'*100)}'`
+echo "Triplet Percentage : $triplet_percent%"
+
