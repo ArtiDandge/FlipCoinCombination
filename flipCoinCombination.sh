4e8337189aef18450dea00637b086f78b087aaf2
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-25 14:36:08 +0530

    display heads or tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x 
 
 echo "This program displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet"
+
+FLIP=$(((RANDOM%10)%2))
+
+if [[ $FLIP -eq 1 ]]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
